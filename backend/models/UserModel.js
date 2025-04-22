fa1847c40c2d299758246c083448257c7a37b423
dheaaavs <162094908+dheaaavs@users.noreply.github.com>
2025-04-22 15:47:52 +0700

    Update UserModel.js

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,35 +1,18 @@
-import { Sequelize, DataTypes } from "sequelize";
+import { Sequelize } from "sequelize";
 import db from "../config/Database.js";
 
-// ==============================
-//           USER MODEL
-// ==============================
+const {DataTypes} = Sequelize;
 
-const User = db.define('users', {
-  username: DataTypes.STRING,
-  password: DataTypes.STRING,
-}, {
-  freezeTableName: true
+const User = db.define('users',{
+    username : DataTypes.STRING,
+    password : DataTypes.STRING,
+
+},{
+    freezeTableName:true
 });
 
-// ==============================
-//          PASIEN MODEL
-// ==============================
+export default User;
 
-const Pasien = db.define('pasien', {
-  nama: Sequelize.STRING,
-  tgl_lahir: Sequelize.STRING,
-  gender: Sequelize.STRING,
-  no_telp: Sequelize.STRING,
-  alamat: Sequelize.STRING,
-}, {
-  freezeTableName: true
-});
-
-// ==============================
-//         SYNC DATABASE
-// ==============================
-
-db.sync().then(() => console.log("Database synced")).catch(err => console.error(err));
-
-export { User, Pasien };
+(async ()=> {
+    await db.sync();
+})();
