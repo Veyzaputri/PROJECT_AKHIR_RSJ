06d5abcd89e800182fd811922a45a580be778891
Shinta Nursobah Chairani <142827052+Shinta505@users.noreply.github.com>
2025-04-22 15:36:18 +0700

    Update UserController.js

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -1,5 +1,7 @@
 import { where } from "sequelize";
 import User from "../models/UserModel.js";
+import Dokter from "../models/UserModel.js";
+import Periksa from "../models/UserModel.js";
 
 export const getUsers = async(req, res) =>{
     try {
@@ -69,3 +71,127 @@ export const loginUser = async (req, res) => {
     res.status(500).json({ msg: "Server error" });
   }
 };
+
+export const getDokter = async (req, res) => {
+    try {
+        const response = await Dokter.findAll();
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const getDokterById = async (req, res) => {
+    try {
+        const doctor = await Dokter.findOne({
+            where: { id_dokter: req.params.id_dokter }
+        });
+
+        if (!doctor) {
+            return res.status(404).json({ msg: "Dokter tidak ditemukan" });
+        }
+
+        res.status(200).json(doctor);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ msg: "Terjadi kesalahan server" });
+    }
+}
+
+export const createDokter = async (req, res) => {
+    try {
+        const inputResult = req.body;
+        await Dokter.create(inputResult);
+        res.status(201).json({ msg: "Doctor created" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const updateDokter = async (req, res) => {
+    try {
+        await Dokter.update(req.body, {
+            where: {
+                id_dokter: req.params.id_dokter
+            }
+        });
+        res.status(200).json({ msg: "This Doctor Updated" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const deleteDokter = async (req, res) => {
+    try {
+        await Dokter.destroy({
+            where: {
+                id_dokter: req.params.id_dokter
+            }
+        });
+        res.status(200).json({ msg: "This Doctor Completed Detele" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const getPeriksa = async (req, res) => {
+    try {
+        const response = await Periksa.findAll();
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const getPeriksaById = async (req, res) => {
+    try {
+        const checkup = await Periksa.findOne({
+            where: { id_periksa: req.params.id_periksa }
+        });
+
+        if (!checkup) {
+            return res.status(404).json({ msg: "Checkup tidak ditemukan" });
+        }
+
+        res.status(200).json(doctor);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ msg: "Terjadi kesalahan server" });
+    }
+}
+
+export const createPeriksa = async (req, res) => {
+    try {
+        const inputResult = req.body;
+        await Periksa.create(inputResult);
+        res.status(201).json({ msg: "Checkup created" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const updatePeriksa = async (req, res) => {
+    try {
+        await Periksa.update(req.body, {
+            where: {
+                id_periksa: req.params.id_periksa
+            }
+        });
+        res.status(200).json({ msg: "This Checkup Updated" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const deletePeriksa = async (req, res) => {
+    try {
+        await Periksa.destroy({
+            where: {
+                id_periksa: req.params.id_periksa
+            }
+        });
+        res.status(200).json({ msg: "This Checkup Completed Detele" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
