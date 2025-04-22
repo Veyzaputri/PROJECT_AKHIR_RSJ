95f6d291d2f6762015fd1b548e359fac60d9473c
dheaaavs <162094908+dheaaavs@users.noreply.github.com>
2025-04-22 15:56:28 +0700

    Update App.js

--- frontend_bc/src/App.js
+++ frontend_bc/src/App.js
@@ -1,17 +1,30 @@
 // src/App.js
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
+
+// User components
 import LoginUser from "./components/LoginUser";
 import AddUser from "./components/AddUser";
 import UserList from "./components/UserList";
+
+// Pasien components
+import PasienList from "./components/PasienList";
+import AddPasien from "./components/AddPasien";
+import EditPasien from "./components/EditPasien";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginUser />} />           
-        <Route path="/register" element={<AddUser />} />     
-        <Route path="/users" element={<UserList />} />       
+        {/* User Routes */}
+        <Route path="/" element={<LoginUser />} />
+        <Route path="/register" element={<AddUser />} />
+        <Route path="/users" element={<UserList />} />
+
+        {/* Pasien Routes */}
+        <Route path="/pasien" element={<PasienList />} />
+        <Route path="/add-pasien" element={<AddPasien />} />
+        <Route path="/edit-pasien/:id" element={<EditPasien />} />
       </Routes>
     </BrowserRouter>
   );
