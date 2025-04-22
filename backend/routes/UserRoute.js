45e8fa3d03d4e142d2d4be46250dead2ac95d567
dheaaavs <162094908+dheaaavs@users.noreply.github.com>
2025-04-22 15:51:17 +0700

    Update UserRoute.js

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,17 +1,38 @@
 import express from "express";
 import {
-    createUser,
-    deleteUser,
-    getUsers, 
-    getUsersById,
-    loginUser,
-
+  createUser,
+  deleteUser,
+  getUsers,
+  getUsersById,
+  loginUser,
 } from "../controller/UserController.js";
 
+import {
+  getPasien,
+  getPasienById,
+  createPasien,
+  updatePasien,
+  deletePasien,
+} from "../controllers/CatatanController.js";
+
 const router = express.Router();
-router.get('/users', getUsers);
-router.get('/users/:id', getUsersById);
-router.post('/users', createUser);
-router.delete('/users/:id', deleteUser);
-router.post('/login', loginUser);
+
+// ==============================
+//             USER
+// ==============================
+router.get("/users", getUsers);
+router.get("/users/:id", getUsersById);
+router.post("/users", createUser);
+router.delete("/users/:id", deleteUser);
+router.post("/login", loginUser);
+
+// ==============================
+//            PASIEN
+// ==============================
+router.get("/pasien", getPasien);
+router.get("/pasien/:id", getPasienById);
+router.post("/add-pasien", createPasien);
+router.put("/pasien/:id", updatePasien);
+router.delete("/pasien/:id", deletePasien);
+
 export default router;
