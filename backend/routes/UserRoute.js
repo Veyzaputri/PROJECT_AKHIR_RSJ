3ab659f5b633ce6cb38e567590b96c3489ff5b94
Shinta Nursobah Chairani <142827052+Shinta505@users.noreply.github.com>
2025-04-22 15:37:36 +0700

    Update UserRoute.js

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -5,13 +5,36 @@ import {
     getUsers, 
     getUsersById,
     loginUser,
-
+    getDokter,
+    getDokterById,
+    createDokter,
+    updateDokter,
+    deleteDokter,
+    getPeriksa,
+    getPeriksaById,
+    createPeriksa,
+    updatePeriksa,
+    deletePeriksa,
 } from "../controller/UserController.js";
 
 const router = express.Router();
+
 router.get('/users', getUsers);
 router.get('/users/:id', getUsersById);
 router.post('/users', createUser);
 router.delete('/users/:id', deleteUser);
 router.post('/login', loginUser);
+
+router.get("/doctor", getDokter);
+router.get("/dokter/:id_dokter", getDokterById);
+router.post("/add-doctor", createDokter);
+router.put("/dokter/:id_dokter", updateDokter);
+router.delete("/dokter/:id_dokter", deleteDokter);
+
+router.get("/checkup", getPeriksa);
+router.get("/periksa/:id_periksa", getPeriksaById);
+router.post("/add-checkup", createPeriksa);
+router.put("/periksa/:id_periksa", updatePeriksa);
+router.delete("/periksa/:id_periksa", deletePeriksa);
+
 export default router;
