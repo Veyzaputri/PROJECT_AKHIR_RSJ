449227c16cfd4212269a31d8594490b966c79215
Shinta Nursobah Chairani <142827052+Shinta505@users.noreply.github.com>
2025-04-22 15:29:28 +0700

    Update UserModel.js
    dokter dan periksa

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -11,7 +11,50 @@ const User = db.define('users',{
     freezeTableName:true
 });
 
+const Dokter = dbContext.define('dokter', {
+    id_dokter: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nama_dokter: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    spesialis: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    tableName: 'dokter',
+    freezeTableName: true,
+    timestamps: false
+});
+
+const Periksa = dbContext.define('periksa', {
+    id_periksa: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    tanggal_periksa: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+    },
+    biaya_periksa: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    tableName: 'periksa',
+    freezeTableName: true,
+    timestamps: false
+});
+
 export default User;
+export { Dokter };
+export { Periksa };
 
 (async ()=> {
     await db.sync();
