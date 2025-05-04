7161354dc48ca8bfbb3f81d959631866a1640616
dheaaavs <ddea8524@gmail.com>
2025-05-04 10:39:07 +0700

    push hasil veyza

---
+++ controller/UserController.js
@@ -0,0 +1,71 @@
+import { where } from "sequelize";
+import User from "../models/UserModel.js";
+export const getUsers = async(req, res) =>{
+    try {
+        const response = await User.findAll();
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+export const getUsersById = async(req, res) =>{
+    try {
+        const response = await User.findOne({
+            where:{
+                id: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const createUser = async(req, res) =>{
+    try {
+        await User.create(req.body);
+        res.status(201).json({msg:"User Created"});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const deleteUser = async(req, res) =>{
+    try {
+        await User.destroy({
+            where:{
+                id: req.params.id
+            }
+        });
+        res.status(200).json({msg:"User Deleted"});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({
+      where: {
+        username: username,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+
+    // Cek password (tanpa bcrypt, langsung cocokkan)
+    if (user.password !== password) {
+      return res.status(401).json({ msg: "Incorrect password" });
+    }
+
+    // Jika login sukses
+    res.status(200).json({ msg: "Login successful", user: user });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import UserRoute from "./routes/UserRoute.js";
+import PasienRoute from "./routes/PasienRoute.js";
+import DokterRoute from "./routes/DokterRoute.js";
+import PeriksaRoute from "./routes/PeriksaRoute.js";
+import ObatRoute from "./routes/ObatRoute.js"
+import StrukRoute from "./routes/StrukRoute.js";
+
+const app = express();
+app.use(cors());
+import { getUsers } from "./controller/UserController.js";
+
+app.use(express.json());
+app.use(UserRoute);
+app.use(PasienRoute);
+app.use(DokterRoute);
+app.use(PeriksaRoute);
+app.use(ObatRoute);
+app.use(StrukRoute);
+
+app.get("/", async (req, res) => {
+    try {
+        await getUsers(req, res); // Langsung panggil fungsi getUsers
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error", error });
+    }
+});
+
+app.listen(5000, "0.0.0.0", ()=> console.log('Server up and running...'));
